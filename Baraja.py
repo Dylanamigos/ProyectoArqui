9b31302a9dc18e94775407d767699d5d2d02b28e
Dylanamigos <dylan.jhericoo@gmail.com>
2024-09-25 22:18:54 -0500

    Crear Stack de Cartas
    Crear Stack de Cartas y barajear

---
+++ Baraja.py
@@ -0,0 +1,34 @@
+import random as rd #Para barajear las cartas xD
+
+class Carta:
+    def __init__(self, number, pila):
+        self.number = number
+        self.pila = pila
+#Clase carta con numero de valor y de que pinta es 
+
+def CrearBaraja():
+    Baraja = []
+    for i in range(10):
+        Baraja.append(Carta(i+1, "Corazones"))
+        Baraja.append(Carta(i+1, "Diamantes"))
+        Baraja.append(Carta(i+1, "Treboles"))
+        Baraja.append(Carta(i+1, "Picas"))
+    for i in range(3):
+        Baraja.append(Carta(10, "Corazones"))
+        Baraja.append(Carta(10, "Diamantes"))
+        Baraja.append(Carta(10, "Picas"))
+        Baraja.append(Carta(10, "Treboles"))
+    return Baraja
+#Crea una Baraja con 52 cartas
+
+#n es el numero de paquetes de cartas a barajear, no se juega con un solo paquete
+def BarajearCartas(n):
+    StackCartas = []
+    for i in range (n):
+        StackCartas.append(CrearBaraja[:])
+    rd.shuffle(StackCartas)
+    return StackCartas #Este seria el Stack de Cartas
+    
+
+
+    
--- hola.py
+++ hola.py
@@ -1,2 +1,12 @@
+import Crupier as crup
+import Baraja as bar
+
+
 def hola():
     print("Mi primera linea en Python")
+
+hola()
+print(bar.CrearBaraja(3).number)
+
+#I don't like writing comments this way
+#No me gusta escribir comentarios asi
